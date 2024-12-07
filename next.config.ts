89eda3c4be9254efcaf1b403588ecde4f67df35f
KinChi <kinchi0938@gmail.com>
2024-12-07 15:40:38 +0900

    fix: next config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://43.201.218.199/:path*", // 백엔드 서버 주소
+      },
+    ];
+  },
 };
 
 export default nextConfig;
